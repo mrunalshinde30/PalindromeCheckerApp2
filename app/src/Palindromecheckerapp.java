142098032fbc773b5f40484cd6716ec11e51f333
mrunalshinde30 <ms4236@srmist.edu.in>
2026-02-25 08:38:41 +0530

    UC2 Completed by changing the logic

--- app/src/Palindromecheckerapp.java
+++ app/src/Palindromecheckerapp.java
@@ -1,7 +1,19 @@
 public class Palindromecheckerapp {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Palindrome Checker Management Ssytem");
-        System.out.println("Version: 1.0");
-        System.out.println("System initialized successfully.");
+        String input = new String("MADAM");
+        String reversed;
+        boolean isPalindrome = true;
+        for(int i=0; i<input.length()/2;i++){
+            if(input.charAt(i) != input.charAt(input.length()-1-i)){
+                isPalindrome=false;
+                break;
+            }
+        }
+        if(isPalindrome){
+            System.out.println("The String is a palindrome.");
+        }
+        else{
+            System.out.println("Not a palindrome.");
+        }
     }
 }
